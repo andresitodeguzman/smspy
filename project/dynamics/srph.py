1a081ff64ee42913bab18fa767ab88e7b4535717
Andresito de Guzman <deguzman.andresito@gmail.com>
2023-01-06 20:53:53 +0800

    fix: fix loop response

--- project/dynamics/srph.py
+++ project/dynamics/srph.py
@@ -7,6 +7,7 @@
 import requests
 
 def do(n,b):
+    defaultResponse = "To report text 'REPORT<space>Your Report' and send to this number"
     # Declare Keyword (important)
     keywordEmpty = "REPORT"
     keyword = "REPORT "
@@ -15,8 +16,10 @@ def do(n,b):
         result = requests.post('https://us-central1-junksimreg-prod.cloudfunctions.net/smsFulfillment', data={ 'message': b, 'number': n, 'code': 24680 })
         print(result)
         return "Thank you! We received your report."
+    elif defaultResponse in b:
+        return False
     elif keywordEmpty in b:
-        return "To report text 'REPORT<space>Your Report' and send to this number"
+        return defaultResponse
     else:
         # Returns a False if Keyword not in Body (important) 
         return False
